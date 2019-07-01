975658d28fabfbf46796650d08da968a9c523129
Kevin.h <563837686@qq.com>
2019-07-01 21:50:52 +0800

    Update 0021.cpp

--- 0021.合并两个有序链表/0021.cpp
+++ 0021.合并两个有序链表/0021.cpp
@@ -6,6 +6,27 @@
 *     ListNode(int x) : val(x), next(NULL) {}
 * };
 */
+class Solution_demo {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* prehead = new ListNode(-1);
+        ListNode* prev = prehead;
+        while(l1 != NULL && l2 != NULL) {
+            if(l1->val <= l2->val) {
+                prev->next = l1;
+                l1 = l1->next;
+            } else {
+                prev->next = l2;
+                l2 = l2->next;
+            }
+            prev = prev->next;
+        }
+        prev->next = l1 != NULL ? l1 : l2;
+
+        return prehead->next;
+    }
+};
+
 class Solution {
 public:
 	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
