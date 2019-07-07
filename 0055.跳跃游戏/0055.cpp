154254e843a5c53456b96608f7a4ea8cdfbd6193
Kevin.h <563837686@qq.com>
2019-07-07 23:57:34 +0800

    Add files via upload

---
+++ 0055.跳跃游戏/0055.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	bool canJump(vector<int>& nums) {
+		int n = nums.size();
+		int maxR = 0;
+		for (int i = 0; i<n; ++i) {
+			if (i > maxR)
+				return false;
+			maxR = max(maxR, i + nums[i]);
+		}
+		return true;
+	}
+};
