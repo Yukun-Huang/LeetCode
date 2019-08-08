4605ed4d750467c9f01d304e74ff24b3b91b1065
Kevin.h <563837686@qq.com>
2019-08-08 13:46:19 +0800

    Add files via upload

---
+++ 0169.求众数/0169.cpp
@@ -0,0 +1,20 @@
+// Boyer-Moore ͶƱ�㷨
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = nums[0], count = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            }
+            else if (nums[i] == candidate) {
+                count++;
+            }
+            else{
+                count--;
+                }
+        }
+        return candidate;
+    }
+};
